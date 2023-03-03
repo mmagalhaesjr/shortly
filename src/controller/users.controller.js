3ea1113e360b135f0cc7ce1e88e099c0e00cf869
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-03-03 16:39:37 -0300

    feat: GET/ranking

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -33,5 +33,30 @@ res.status(200).send(userUrls)
 };
 
 export async function ranking(req,res){
+const {rows} = await db.query(`
+SELECT json_build_object(
+	'id', users.id,
+	'name', users.name,
+	'linksCount', (CASE WHEN total."linksCount" IS NOT NULL THEN total."linksCount" ELSE 0 END),
+	'visitCount', (CASE WHEN total."visitCount" IS NOT NULL THEN total."visitCount" ELSE 0 END)
+	) AS user_info
 
+FROM users
+LEFT JOIN urls ON users.id = urls."userId"
+LEFT JOIN (
+	SELECT urls."userId" as "userId", COUNT(*) as "linksCount", SUM(urls."visitCount") as "visitCount" 
+	FROM urls 
+	GROUP BY urls."userId"
+) AS total ON total."userId" = users.id
+
+GROUP BY users.id, total."visitCount", total."linksCount"
+
+ORDER BY COALESCE(total."visitCount", 0) DESC, COALESCE(total."linksCount", 0) DESC, users.id DESC
+LIMIT 10
+
+`)
+
+const result = rows.map( i => i.user_info )
+
+res.status(200).send(result)
 };
